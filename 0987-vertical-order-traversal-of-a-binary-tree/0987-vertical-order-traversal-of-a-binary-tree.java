f1f0deadb4f5b42fd105218056b934fd6142f1fb
Naman Katare <142571865+27Naman2004@users.noreply.github.com>
2026-08-07 00:00:10 +0530

    Time: 4 ms (60.86%), Space: 44.4 MB (17.4%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -13,40 +13,49 @@
  *     }
  * }
  */
-
 class Solution {
     class Pair{
-        int level, hd;
+        int height;
+        int level;
         TreeNode root;
-        Pair(int level, int hd, TreeNode root){
-            this.level = level;
-            this.hd = hd;
+        Pair(TreeNode root, int level, int height){
             this.root = root;
+            this.level = level;
+            this.height = height;
         }
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root == null) return ans;
+
+        List<List<Integer>> ans = new  ArrayList<>();
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
 
         Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(0,0, root));
-
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        q.offer(new Pair(root, 0, 0));
 
         while(!q.isEmpty()){
             int s = q.size();
-            Pair curr = q.poll();
 
-            map.putIfAbsent(curr.hd, new TreeMap<>());
-            map.get(curr.hd).putIfAbsent(curr.level, new PriorityQueue<>());
-            map.get(curr.hd).get(curr.level).add(curr.root.val);
+            
+            for(int i=0;i<s;i++){
+                Pair curr = q.poll();
 
-            if(curr.root.left != null) q.offer(new Pair(curr.level+1,curr.hd-1,curr.root.left));
-            if(curr.root.right != null) q.offer(new Pair(curr.level+1,curr.hd+1, curr.root.right));
+                int level = curr.level;
+                int height = curr.height;
+                TreeNode node = curr.root;
 
+                map.putIfAbsent(level, new TreeMap<>());
+                map.get(level).putIfAbsent(height, new PriorityQueue<>());
+                map.get(level).get(height).add(node.val);
+
+                if(node.left != null){
+                    q.offer(new Pair(node.left, level-1, height+1));
+                }
+                if(node.right != null){
+                    q.offer(new Pair(node.right, level+1, height+1));
+                }
+            }
         }
-
+        
         for(var i: map.values()){
             List<Integer> temp = new ArrayList<>();
             for(var j: i.values()){
